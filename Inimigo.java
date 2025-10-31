c5e139e010c9092d3fde5dcc7e489ca57229a962
HenryPiozzi <103003294+HenryPiozzi@users.noreply.github.com>
2025-10-31 20:34:53 -0300

    funcionar upar 2 vezes

--- Inimigo.java
+++ Inimigo.java
@@ -7,7 +7,7 @@ public class Inimigo extends Personagem implements Cloneable {
     }
 
     // Método estático para gerar um inimigo aleatório baseado no capítulo atual
-    public static Inimigo gerarInimigo(int capituloAtual) {
+    public static Inimigo gerarInimigo(int capituloAtual, byte inimigosDerrotados) {
         String[] nomesBase = {"Goblin", "Orc", "Esqueleto", "Lobo", "Bandido", "Mago Negro", "Troll", "Zumbi"};
 
         String nome = nomesBase[ThreadLocalRandom.current().nextInt(nomesBase.length)];
@@ -19,7 +19,7 @@ public class Inimigo extends Personagem implements Cloneable {
         int pontosVida = vidaBase + (capituloAtual * ThreadLocalRandom.current().nextInt(5, 15));
         int ataque = ataqueBase + (capituloAtual * ThreadLocalRandom.current().nextInt(1, 4));
         int defesa = defesaBase + (capituloAtual * ThreadLocalRandom.current().nextInt(1, 3));
-        byte nivel = (byte) capituloAtual;
+        byte nivel = inimigosDerrotados;
 
         try {
             return new Inimigo(nome, (short)pontosVida, (short)ataque, (short)defesa, nivel, new Inventario());
